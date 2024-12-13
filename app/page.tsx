b5db833959072e8998f460f9a765c3b0ba010e0d
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2024-12-13 19:43:14 +0000

    uploadthing added!

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
-import { currentUser } from "@clerk/nextjs/server";
+"use client";
 
-export default async function Home() {
-  const user = await currentUser();
-  return <main>Hello {user?.fullName}</main>;
+import { UploadButton, UploadDropzone } from "@/utils/uploadthing";
+import { useUser } from "@clerk/nextjs";
+
+export default function Home() {
+  const { isLoaded, user } = useUser();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {isLoaded && user && (
+        <p className="text-2xl mb-10">hey👋 {user.fullName}</p>
+      )}
+      <UploadDropzone
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </main>
+  );
 }
